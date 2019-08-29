25630f0ea9b35cb3a550a63f49b6584d3e3063ad
Don Moore <donsmoore@gmail.com>
2019-08-29 13:54:35 -0500

    adding db connection

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	_ "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	_ "go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	// connect db
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	// get table
+	collection := client.Database("testing").Collection("numbers")
+
+	// insert a row
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	res, _ := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159})
+
+	// show new row id
+	id := res.InsertedID
+	fmt.Println("Done: ",  id , " success.")
+
 }
